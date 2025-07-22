5820fac177714bc4aef1ac5c8220ad2cbf59f1af
salimlaghrib <salim16laghrib@gmail.com>
2025-07-22 23:27:36 +0100

    feat : add splash screen

---
+++ lib/views/screens/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'package:fanradar/core/routes/app_routes.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.offNamed(
+        AppRoutes.home,
+      ); // Remplace AppRoutes.home par ce que tu veux
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Image.asset('assets/images/FanRadar.png', width: 200)],
+        ),
+      ),
+    );
+  }
+}
